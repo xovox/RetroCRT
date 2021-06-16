3fe043d35d5f5251d93ad4b3ca48040eb59d3a47
Duncan <20759483+xovox@users.noreply.github.com>
2021-06-16 15:38:55 +0100

    removing wpa nonsense

--- files/autostart.sh
+++ files/autostart.sh
@@ -5,13 +5,6 @@ wpa_conf=/etc/wpa_supplicant/wpa_supplicant.conf
 
 eval "$(dos2unix < "/boot/retrocrt/retrocrt.txt")"
 
-#if [ -f $wpa_rcrt ] ; then
-#	sudo mv $wpa_rcrt $wpa_conf
-#	sudo chmod 400 $wpa_conf
-#	sudo chown root.root $wpa_conf
-#	sudo systemctl restart wpa_supplicant
-#fi &
-
 if [[ "$rotate_es" == [13] ]]; then
 	resolution_es="240 320"
 else
